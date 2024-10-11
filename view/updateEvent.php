7ed76d352f3e109c8d1e9f18065720b2db72717a
raonych <raonych@gmail.com>
2024-10-10 21:57:06 -0300

    correção de erros

--- view/updateEvent.php
+++ view/updateEvent.php
@@ -8,9 +8,9 @@ echo '<form action="../controller/eventUpdate.php"method="post"> ';
 echo '<input type="text" name="eventName" value="',$event['eventName'] ,'"></input>';
 echo '<input type="date" name="eventDate" value="',$event['eventDate'] ,'"></input>';
 echo '<input type="time" name="eventInit" value="',$event['eventInit'] ,'"></input>';
-echo '<input type="time" name="eventInit" value="',$event['eventEnd'] ,'"></input>';
-echo '<input type="text" name="eventInit" value="',$event['eventDesc'] ,'"></input>';
-echo '<input type="text" name="eventInit" value="',$event['eventRes'] ,'"></input>';
+echo '<input type="time" name="eventEnd" value="',$event['eventEnd'] ,'"></input>';
+echo '<input type="text" name="eventDesc" value="',$event['eventDesc'] ,'"></input>';
+echo '<input type="text" name="eventRes" value="',$event['eventRes'] ,'"></input>';
 echo "<input type='hidden' name='event' value='$id'>";
 
 echo '<button type="submit">Salvar alterações</button>';
