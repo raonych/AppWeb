96374a9e90953599ebd730531c8a1940f4a568d9
Raony Chagas <raonych@gmail.com>
2024-10-22 23:03:00 -0300

    modificando header

--- includes/header.php
+++ includes/header.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Planner</title>
-  <link rel="stylesheet" href="static/css/main.css">
+    <link rel="stylesheet" href="static/css/main.css">
 </head>
 <body>
 <div class="header">
     <a href="index.php">home</a>
     <?php include 'includes/searchBar.php'; ?>
-    <a class="event-new" href="?pagina=new">Novo evento</a>
+    <a class="event-new" href="?pagina=new"><?php include "static/svg/plus.html"?> Novo evento</a>
 </div>
+</body>
+</html>
