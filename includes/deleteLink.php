f38cfbe5f86c8fd83180f4ed99c956b8a1dcc4cf
Raony Chagas <raonych@gmail.com>
2024-10-25 19:31:10 -0300

    Update deleteLink.php

--- includes/deleteLink.php
+++ includes/deleteLink.php
@@ -5,7 +5,6 @@ if(isset($_POST['event'])){
 echo "<div class='deleteButton'>";
 echo "<form action='./controller/eventDelete.php' method='post'>";
 echo "<input type='hidden' name='event' value='$id'>";
-echo "<input type='hidden' name='pagina' value='delete'>";
 echo '<button type="submit">';
      include 'static/svg/trashCan.html';
       '</button>';
